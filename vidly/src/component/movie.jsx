ce795fe95dd17e8b34df9594111f4098716a0ef0
Fraser Carpenter <fraser@hqtech.co.za>
2020-12-10 15:06:05 +0200

    clean up

--- vidly/src/component/movie.jsx
+++ vidly/src/component/movie.jsx
@@ -1,25 +1,5 @@
 import React, { Component } from 'react';
 
-// class Movie extends Component {
-//   handleSave = () => {
-//     this.props.history.replace('/movies');
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Movie - {this.props.match.params.Id}</h1>
-//         <button
-//           className="btn btn-primary btn-sm m-2"
-//           onClick={() => this.handleSave()}
-//         >
-//           Save
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
 const Movie = ({ match, history }) => {
   return (
     <div>
